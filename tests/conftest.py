13f12bafb2213946f09f2ebe67916503be906a93
prajes <rajesw.priya@gmail.com>
2019-08-27 10:31:43 +0000

    Mise en place de l'environnement et dossier pour les tests

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+import sys 
+
+sys.path.append('.')
+
+from FYN import app as FYNapp
+
+
+@pytest.fixture
+def app():
+    app = FYNapp
+    return app 
+
+@pytest.fixture
+def client():
+    app = FYNapp
+    client = app.test_client()
+    yield client 
