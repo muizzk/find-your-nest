6342af4e514b2ceadd69cfe5f5931e37e148f702
priya <rajesw.priya@gmail.com>
2019-08-29 16:19:44 +0200

    Correction de l'indentation dans la partie infos_compte

--- FYN/views.py
+++ FYN/views.py
@@ -202,51 +202,53 @@ def Fiche(id):
 @app.route("/infoscompte/")
 @login_required
 def infoscompte():
- if current_user.is_authenticated:
-  print(current_user.pro)
-  if current_user.pro == 'on':
-   email = current_user.id
-   infos_pro = c.execute("select prenom, email, temps, budget, maison, appartement, id_utilisateur FROM utilisateur where email=?", (email,)).fetchone() 
-   maison = infos_pro[4]
-   appartement = infos_pro[5]
-   infos_adresse = c.execute("select nb, rue, ville, code_postal from adresse inner join utilisateur on adresse.id_adresse=utilisateur.id_adresse where email=?", (email,)).fetchone()
-   if maison == 'on':
-    type_logement = 'maison'
-   elif appartement == 'on':
-    type_logement = 'appartement'
-   else:
-    type_logement = 'Non précisé'
-   return render_template("infoscomptepro.html", prenom=infos_pro[0], email=infos_pro[1], temps=infos_pro[2], budget=infos_pro[3], type_logement=type_logement, nb=infos_adresse[0], rue=infos_adresse[1], ville=infos_adresse[2], code_postal=infos_adresse[3])
+    if current_user.is_authenticated:
+        print(current_user.pro)
+        if current_user.pro == 'on':
+            email = current_user.id
+            infos_pro = c.execute("select prenom, email, temps, budget, maison, appartement, id_utilisateur FROM utilisateur where email=?", (email,)).fetchone() 
+            maison = infos_pro[4]
+            appartement = infos_pro[5]
+            infos_adresse = c.execute("select nb, rue, ville, code_postal from adresse inner join utilisateur on adresse.id_adresse=utilisateur.id_adresse where email=?", (email,)).fetchone()
+            if maison == 'on':
+                type_logement = 'maison'
+            elif appartement == 'on':
+                type_logement = 'appartement'
+            else:
+                type_logement = 'Non précisé'
 
-  else :
-   email = current_user.id
-   infos_pro = c.execute("select prenom, email, temps, budget, maison, appartement, id_utilisateur FROM utilisateur where email=?", (email,)).fetchone() 
-   maison = infos_pro[4]
-   appartement = infos_pro[5]
-   infos_adresse = c.execute("select nb, rue, ville, code_postal from adresse inner join utilisateur on adresse.id_adresse=utilisateur.id_adresse where email=?", (email,)).fetchone()
-   id_user=infos_pro[6]
-   infos_favoris = c.execute("SELECT titre, prix, photo, description from logement inner join favoris on logement.id_logement=favoris.id_logement where favoris.id_utilisateur=?", (id_user,)).fetchone()
-   if infos_favoris is None:
-       if maison == 'on' and appartement !='on':
-           type_logement = 'maison'
-       elif appartement == 'on' and maison != 'on':
-           type_logement = 'appartement'
-       elif appartement == 'on' and maison =='on':
-           type_logement = 'Maison et appartement'
-       else:
-           type_logement = 'Non précisé'
-           return render_template("infoscompte.html", prenom=infos_pro[0], email=infos_pro[1], temps=infos_pro[2], budget=infos_pro[3], type_logement=type_logement, nb=infos_adresse[0], rue=infos_adresse[1], ville=infos_adresse[2], code_postal=infos_adresse[3])
+            return render_template("infoscomptepro.html", prenom=infos_pro[0], email=infos_pro[1], temps=infos_pro[2], budget=infos_pro[3], type_logement=type_logement, nb=infos_adresse[0], rue=infos_adresse[1], ville=infos_adresse[2], code_postal=infos_adresse[3])
 
-   else:
-       if maison == 'on' and appartement != 'on' :
-           type_logement = 'maison'
-       elif appartement == 'on' and maison != 'on':
-           type_logement = 'appartement'
-       elif appartement =='on' and maison =='on':
-           type_logement = 'Maison et appartement'
-       else:
-           type_logement = 'Non précisé'
-           return render_template("infoscompte.html", prenom=infos_pro[0], email=infos_pro[1], temps=infos_pro[2], budget=infos_pro[3], type_logement=type_logement, nb=infos_adresse[0], rue=infos_adresse[1], ville=infos_adresse[2], code_postal=infos_adresse[3], titre=infos_favoris[0], prix=infos_favoris[1], photo=infos_favoris[2], description=infos_favoris[3])
+        else:
+            email = current_user.id
+            infos_pro = c.execute("select prenom, email, temps, budget, maison, appartement, id_utilisateur FROM utilisateur where email=?", (email,)).fetchone() 
+            maison = infos_pro[4]
+            appartement = infos_pro[5]
+            infos_adresse = c.execute("select nb, rue, ville, code_postal from adresse inner join utilisateur on adresse.id_adresse=utilisateur.id_adresse where email=?", (email,)).fetchone()
+            id_user=infos_pro[6]
+            infos_favoris = c.execute("SELECT titre, prix, photo, description from logement inner join favoris on logement.id_logement=favoris.id_logement where favoris.id_utilisateur=?", (id_user,)).fetchone()
+            if infos_favoris is None:
+                if maison == 'on' and appartement !='on':
+                    type_logement = 'maison'
+                elif appartement == 'on' and maison != 'on':
+                    type_logement = 'appartement'
+                elif appartement == 'on' and maison =='on':
+                    type_logement = 'Maison et appartement'
+                else:
+                    type_logement = 'Non précisé'
+                
+                return render_template("infoscompte.html", prenom=infos_pro[0], email=infos_pro[1], temps=infos_pro[2], budget=infos_pro[3], type_logement=type_logement, nb=infos_adresse[0], rue=infos_adresse[1], ville=infos_adresse[2], code_postal=infos_adresse[3])
 
- else:
-  return redirect(url_for('main'))
+            else:
+                if maison == 'on' and appartement != 'on' :
+                    type_logement = 'maison'
+                elif appartement == 'on' and maison != 'on':
+                    type_logement = 'appartement'
+                elif appartement =='on' and maison =='on':
+                    type_logement = 'Maison et appartement'
+                else:
+                    type_logement = 'Non précisé'
+            return render_template("infoscompte.html", prenom=infos_pro[0], email=infos_pro[1], temps=infos_pro[2], budget=infos_pro[3], type_logement=type_logement, nb=infos_adresse[0], rue=infos_adresse[1], ville=infos_adresse[2], code_postal=infos_adresse[3], titre=infos_favoris[0], prix=infos_favoris[1], photo=infos_favoris[2], description=infos_favoris[3])
+
+    else:
+        return redirect(url_for('main'))
