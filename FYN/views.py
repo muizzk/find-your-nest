9641ad1db7d8030e82b8978901794ed596fac3ad
BeatriceBea <52967526+BeatriceBea@users.noreply.github.com>
2019-08-29 16:45:58 +0200

    Copie de la branche prajes

--- FYN/views.py
+++ FYN/views.py
@@ -1,14 +1,13 @@
 # vim: set ts=4 sw=4 et:
 from FYN import app, login_manager 
-from flask import send_file, request, redirect, render_template, flash, url_for, redirect, flash
+from flask import request, redirect, render_template, flash, url_for, redirect, flash
 from flask_login import login_required, UserMixin, login_user, current_user, logout_user
 from opencage.geocoder import OpenCageGeocode
 from urllib import parse
 import sqlite3, requests, json
 from pathlib import PurePath  
 from passlib.hash import sha256_crypt
-from werkzeug import secure_filename
-
+ 
 #Import Navitia key
 navitia_key = app.config['NAVITIA']
 navitia_url = "http://api.navitia.io/v1/journeys"
@@ -17,12 +16,9 @@ navitia_url = "http://api.navitia.io/v1/journeys"
 opencagedata_key = "3c853893fc37402eb2ef1473b6629218"
 opencage = OpenCageGeocode(opencagedata_key)
 
-database_file = PurePath('./FYN/findyournest.db')
+database_file = PurePath('FYN/findyournest.db')
 conn = sqlite3.connect(str(database_file), check_same_thread=False)
 c = conn.cursor()
-
-#Chargement images dans le dossier
-upload_pro = PurePath ('FYN/ups/')
 
 #loading the login manager
 @login_manager.user_loader
@@ -203,8 +199,6 @@ def Fiche(id):
     surface_sql =  c.execute("SELECT superficie FROM logement WHERE id_logement=?", (id,)).fetchone()
     return render_template("FicheAppart.html", Prix=prix_sql[0], PostalCode=PostalCode_sql[0], nb_pieces=nb_pieces_sql[0], surface=surface_sql[0])
 
-#Partie pro
-
 @app.route("/infoscompte/")
 @login_required
 def infoscompte():
@@ -212,7 +206,7 @@ def infoscompte():
         print(current_user.pro)
         if current_user.pro == 'on':
             email = current_user.id
-            infos_pro = c.execute("select prenom, email, temps, budget, maison, appartement, id_utilisateur FROM utilisateur where email=?", (email,)).fetchone()    
+            infos_pro = c.execute("select prenom, email, temps, budget, maison, appartement, id_utilisateur FROM utilisateur where email=?", (email,)).fetchone() 
             maison = infos_pro[4]
             appartement = infos_pro[5]
             infos_adresse = c.execute("select nb, rue, ville, code_postal from adresse inner join utilisateur on adresse.id_adresse=utilisateur.id_adresse where email=?", (email,)).fetchone()
@@ -222,26 +216,42 @@ def infoscompte():
                 type_logement = 'appartement'
             else:
                 type_logement = 'Non précisé'
+
             return render_template("infoscomptepro.html", prenom=infos_pro[0], email=infos_pro[1], temps=infos_pro[2], budget=infos_pro[3], type_logement=type_logement, nb=infos_adresse[0], rue=infos_adresse[1], ville=infos_adresse[2], code_postal=infos_adresse[3])
 
         else:
-            email =  current_user.id
-            infos_pro = c.execute("select prenom, email, temps, budget, maison, appartement, id_utilisateur FROM  utilisateur where email=?", (email,)).fetchone()    
+            email = current_user.id
+            infos_pro = c.execute("select prenom, email, temps, budget, maison, appartement, id_utilisateur FROM utilisateur where email=?", (email,)).fetchone() 
             maison = infos_pro[4]
             appartement = infos_pro[5]
             infos_adresse = c.execute("select nb, rue, ville, code_postal from adresse inner join utilisateur on adresse.id_adresse=utilisateur.id_adresse where email=?", (email,)).fetchone()
             id_user=infos_pro[6]
             infos_favoris = c.execute("SELECT titre, prix, photo, description from logement inner join favoris on logement.id_logement=favoris.id_logement where favoris.id_utilisateur=?", (id_user,)).fetchone()
-            if maison == 'on':
-                type_logement = 'maison'
-            elif appartement == 'on':
-                type_logement = 'appartement'
+            if infos_favoris is None:
+                if maison == 'on' and appartement !='on':
+                    type_logement = 'maison'
+                elif appartement == 'on' and maison != 'on':
+                    type_logement = 'appartement'
+                elif appartement == 'on' and maison =='on':
+                    type_logement = 'Maison et appartement'
+                else:
+                    type_logement = 'Non précisé'
+                
+                return render_template("infoscompte.html", prenom=infos_pro[0], email=infos_pro[1], temps=infos_pro[2], budget=infos_pro[3], type_logement=type_logement, nb=infos_adresse[0], rue=infos_adresse[1], ville=infos_adresse[2], code_postal=infos_adresse[3])
+
             else:
-                type_logement = 'Non précisé'
-                return render_template("infoscompte.html", prenom=infos_pro[0], email=infos_pro[1], temps=infos_pro[2], budget=infos_pro[3], type_logement=type_logement, nb=infos_adresse[0], rue=infos_adresse[1], ville=infos_adresse[2], code_postal=infos_adresse[3], titre=infos_favoris[0], prix=infos_favoris[1], photo=infos_favoris[2], description=infos_favoris[3])
+                if maison == 'on' and appartement != 'on' :
+                    type_logement = 'maison'
+                elif appartement == 'on' and maison != 'on':
+                    type_logement = 'appartement'
+                elif appartement =='on' and maison =='on':
+                    type_logement = 'Maison et appartement'
+                else:
+                    type_logement = 'Non précisé'
+            return render_template("infoscompte.html", prenom=infos_pro[0], email=infos_pro[1], temps=infos_pro[2], budget=infos_pro[3], type_logement=type_logement, nb=infos_adresse[0], rue=infos_adresse[1], ville=infos_adresse[2], code_postal=infos_adresse[3], titre=infos_favoris[0], prix=infos_favoris[1], photo=infos_favoris[2], description=infos_favoris[3])
+
     else:
         return redirect(url_for('main'))
-
 def checkextension(namefile):
     """ Renvoie True si le fichier possède une extension d'image valide. """
     print(namefile.rsplit('.', 1)[1])
